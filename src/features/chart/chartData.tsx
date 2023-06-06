d728636bdc1cfebea36836380cd7758216d24279
sapna-sarvaiya <sapna.sarvaiya@scaletech.xyz>
2023-06-06 12:12:52 +0530

    add chart and arrange forecast list

--- src/features/chart/chartData.tsx
+++ src/features/chart/chartData.tsx
@@ -2,26 +2,40 @@ import React from 'react';
 import moment from 'moment';
 import { Line } from 'react-chartjs-2';
 import 'chart.js/auto';
-import { ForecastData } from 'features/weatherTemp/contianer/home';
+
 import { IWeatherChartData } from 'features/weatherTemp/interface/forecast.interface';
-  
+
 
 interface IProps {
-    data: IWeatherChartData[]
+  data: IWeatherChartData[]
 }
 
-const WeatherChart:React.FC<IProps> = (props) => {
-    const {data} = props;
+const WeatherChart: React.FC<IProps> = (props) => {
+  const { data } = props;
   // Prepare the chart data
   const chartData = {
-    labels: data.map((entry: IWeatherChartData ) =>
-    moment(entry?.dt_txt).format('MMM DD YY,  h:mm') ),
+    labels: data.map((entry: IWeatherChartData) =>
+      moment(entry?.dt_txt).format('MMM DD YY,  h:mm')),
     datasets: [
       {
         label: 'Temperature (°C)',
-        data: data.map((entry : IWeatherChartData) => entry.main.temp),
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        borderColor: 'rgba(75, 192, 192, 1)',
+        data: data.map((entry: IWeatherChartData) => entry.main.temp),
+        backgroundColor: 'rgba(0,0,0,0)',
+        borderColor: '	rgb(0,0,0)',
+        borderWidth: 1,
+      },
+      {
+        label: 'Humididty (%)',
+        data: data.map((entry: IWeatherChartData) => entry.main.humidity),
+        backgroundColor: 'rgba(0,0,0,0)',
+        borderColor: 'rgb(139, 0, 0)',
+        borderWidth: 1,
+      },
+      {
+        label: 'wind speed (km/h)',
+        data: data.map((entry: IWeatherChartData) => entry.wind.speed),
+        backgroundColor: 'rgba(0,0,0,0)',
+        borderColor: 'rgb(0, 0, 255)',
         borderWidth: 1,
       },
     ],
@@ -31,18 +45,37 @@ const WeatherChart:React.FC<IProps> = (props) => {
   const chartOptions = {
     scales: {
       y: {
+        display: false,
         beginAtZero: true,
-        suggestedMax: 40, // Customize the maximum value on the y-axis if needed
+        suggestedMax: 40,
+        // Customize the maximum value on the y-axis if needed
+      },
+      x: {
+        ticks: {
+          color: '#000000',
+        }
+        // Customize the maximum value on the y-axis if needed
+      },
+    },
+    plugins: {
+      legend: {
+        position: 'top' as const,
+        labels: {
+          color: "#000000",
+        }
+      },
+      title: {
+        display: true,
       },
     },
   };
 
   return (
     <div className=' mb--20 text--white'>
-        <Line data={chartData} options={chartOptions} />
+      <Line data={chartData} options={chartOptions} />
     </div>
   )
-  
+
 };
 
 export default WeatherChart;
--- src/features/weatherTemp/contianer/home.tsx
+++ src/features/weatherTemp/contianer/home.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 import axios from 'axios';
 import { Line } from 'react-chartjs-2';
-import  ChartApp  from 'features/chart/chartData';
+import ChartApp from 'features/chart/chartData';
 
 import humidity from "../../../assets/images/clockhumidity.png";
 // import { ChartData, ChartOptions } from 'chart.js/auto';
@@ -55,7 +55,7 @@ const WeatherApp: React.FC = () => {
         const data: WeatherData = response.data;
         console.log(`response`, response)
         setWeatherData(data);
-    getForecastData();
+        getForecastData();
       })
       .catch((error) => {
         toast.error(" please enter valid city");
@@ -64,9 +64,9 @@ const WeatherApp: React.FC = () => {
   };
 
   const getForecastData = () => {
-    axios .get<IWeatherChartData[]>(`https://api.openweathermap.org/data/2.5/forecast?&q=${cityName}&cnt=10&units=metric&appid=35529732c40e184ca9f1121b0bf00dc8`).then((response)=> {
+    axios.get<IWeatherChartData[]>(`https://api.openweathermap.org/data/2.5/forecast?&q=${cityName}&cnt=10&units=metric&appid=35529732c40e184ca9f1121b0bf00dc8`).then((response) => {
       const data: IWeatherChartData[] = response.data?.list;
-      
+
       setForeCastData(data);
     })
   }
@@ -78,20 +78,25 @@ const WeatherApp: React.FC = () => {
       {/* <div className='flex'>
         <ChartApp/>
         </div> */}
-      <div className='flex flex--column'>
-      <div className="container">
-        {/* <div id="pot">
+      <div className='flex'>
+        <div className='chart'>
+
+          {forecastData.length > 0 &&
+            <WeatherChart data={forecastData} />}
+        </div>
+        <div className="container">
+          {/* <div id="pot">
           <img src="https://i.stack.imgur.com/qgNyF.png?s=328&g=1" width="100px" height="100px" />
         </div> */}
-        <form onSubmit={handleSubmit}>
-          <div className="searchBox">
-            <input type="search"
-              value={cityName}
-              onChange={handleInputChange}
-              placeholder="Enter city name" autoFocus required />
-            <i className="fa fa-search"></i>
-          </div>
-          {/* <input
+          <form onSubmit={handleSubmit}>
+            <div className="searchBox">
+              <input type="search"
+                value={cityName}
+                onChange={handleInputChange}
+                placeholder="Enter city name" autoFocus required />
+              <i className="fa fa-search"></i>
+            </div>
+            {/* <input
             type="text"
             value={cityName}
             onChange={handleInputChange}
@@ -99,77 +104,75 @@ const WeatherApp: React.FC = () => {
             className='input-width'
           />
           <button type="submit" className='cursor-pointer search-btn '>Search</button> */}
-        </form>
-        <div className='flex justify-content--around'>
+          </form>
+          <div className='flex justify-content--around circle'>
 
-        <div className="mid">
-          <div className="corner_text">
-            <p>N</p>
-            <p>W</p>
-            <p>E</p>
-            <p>S</p>
-            <p>|</p>
-            <p>|</p>
-            <p>|</p>
-            <p>|</p>
-          </div>
-        </div>
-    </div>
-        <div className="data">
-          <div className="cel">
-            <h4>{weatherData?.weather?.[0].description}</h4>
-            {/* <p className="date">{date.getHours}</p> */}
-
-            <p className='flex flex--column'><span>{parseInt(String(weatherData?.main?.temp))}°C</span></p>
-          </div>
-          <div className="max_min">
-            <div className="max">
-              <p className='flex flex--column'>lat <span>{weatherData?.coord?.lat}</span> </p>
-              <hr />
-              <p>Humidity <span>{weatherData?.main?.humidity}%</span></p>
-            </div>
-            <div className="cloud">
-              <img src="https://e7.pngegg.com/pngimages/955/496/png-clipart-sun-and-cloud-digital-illustration-weather-forecasting-rain-icon-shower-weather-icon-material-company-cloud.png" alt="">
-              </img>	
+            <div className="mid">
+              <div className="corner_text">
+                <p>N</p>
+                <p>W</p>
+                <p>E</p>
+                <p>S</p>
+                <p>|</p>
+                <p>|</p>
+                <p>|</p>
+                <p>|</p>
               </div>
-            <div className="min">
-              <p className='flex flex--column'>lon <span>{weatherData?.coord?.lon}</span> </p>
-              <hr />
-              <p>Pressur <span>{weatherData?.main?.pressure}mm</span></p>
             </div>
           </div>
-          <div className="bottom">
-            <p className='flex flex--column'>Wind speed <span>{((weatherData?.wind?.speed) * 3.6).toFixed(4)} km/h</span></p>
-            <h2>{weatherData.name}</h2>
+          <div className="data">
+            <div className="cel mt---32">
+              <h4>{weatherData?.weather?.[0].description}</h4>
+              {/* <p className="date">{date.getHours}</p> */}
+
+              <p className='flex flex--column'><span>{parseInt(String(weatherData?.main?.temp))}°C</span></p>
+            </div>
+            <div className="max_min">
+              <div className="max">
+                <p className='flex flex--column'>lat <span>{weatherData?.coord?.lat}</span> </p>
+                <hr />
+                <p>Humidity <span>{weatherData?.main?.humidity}%</span></p>
+              </div>
+              <div className="cloud">
+                <img src="https://e7.pngegg.com/pngimages/955/496/png-clipart-sun-and-cloud-digital-illustration-weather-forecasting-rain-icon-shower-weather-icon-material-company-cloud.png" alt="">
+                </img>
+              </div>
+              <div className="min">
+                <p className='flex flex--column'>lon <span>{weatherData?.coord?.lon}</span> </p>
+                <hr />
+                <p>Pressur <span>{weatherData?.main?.pressure}mm</span></p>
+              </div>
+            </div>
+            <div className="bottom">
+              <p className='flex flex--column'>Wind speed <span>{((weatherData?.wind?.speed) * 3.6).toFixed(4)} km/h</span></p>
+              <h2>{weatherData.name}</h2>
+            </div>
           </div>
-        </div>
- 
+
+        </div >
+
+
+        {error && <p>{error}</p>}
       </div >
-      <div>
-      {forecastData.length>0 &&
-      <WeatherChart data={forecastData}/>}
-      </div>
-       <div className='forecast-list flex'>
-          {forecastData?.list?.map((item)=> 	<>
+      <div className='forecast-list flex'>
+        {forecastData?.map((item) => <>
           <div className="section">
-		<div className="weather-description">
-            <div className='flex flex--column align-items--center justify-content--center'>
-            <div className='font--extra-bold'>{item?.dt_txt.split(' ')[0]}</div>
-            <div className='font--extra-bold'>{item?.dt_txt.split(' ')[1]}</div>
-            </div>
-			<div className="weather-info flex justify-content--evenly">
-        <div className='inner-div'>
-      <p className='flex flex--column justify-content--center align-items--center mt--10 font--bold'><i className="fa fa-cloud-moon-rain"></i> <h5 className="mt--10">{(parseInt(String(item?.main?.temp)))}°C</h5> </p>
+            <div className="weather-description">
+              <div className='flex flex--column align-items--center justify-content--center'>
+                <div className='font--extra-bold'>{item?.dt_txt.split(' ')[0]}</div>
+                <div className='font--extra-bold'>{item?.dt_txt.split(' ')[1]}</div>
+              </div>
+              <div className="weather-info flex justify-content--evenly">
+                <div className='inner-div'>
+                  <p className='flex flex--column justify-content--center align-items--center mt--10 font--bold'><i className="fa fa-cloud-moon-rain"></i> <h5 className="mt--10">{(parseInt(String(item?.main?.temp)))}°C</h5> </p>
+                </div>
+                <div className='inner-div ml--10'>
+                  <p className='flex flex--column justify-content--center align-items--center mt--10 font--bold'><span><img src={humidity} width='35px' /></span><h5>{item?.main?.humidity}%</h5> </p>
+                </div>
+              </div>
+            </div></div></>
+        )}
       </div>
-      <div className='inner-div'>
-      <p className='flex flex--column justify-content--center align-items--center mt--10 font--bold'><span><img src={humidity} width='35px'/></span><h5>{item?.main?.humidity}%</h5> </p>
-      </div>
-			</div>	
-		</div></div></>
-  )}
-	  </div>
-      {error && <p>{error}</p>}
-    </div >
     </div>
   );
 };
